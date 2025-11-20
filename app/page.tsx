250e8fa1e138edab219456736df4405830834fdf
K3rcy <kmaslowski0@gmail.com>
2025-11-20 17:40:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div>
       <p className={styles.main_p}> 
-        My name is Jakub Masłowski and I'm 16 years old. I am learning programming at 
+        My name is Jakub Masłowski and I&apos;m 16 years old. I am learning programming at 
         Zespół Szkół Technicznych w Wodzisławiu Śląskim. I enjoy programming and learning 
         new technologies. I like playing sports, cooking and listening to music.
       </p>
